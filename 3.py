5fc56e8e51d4fa6da590507eca472f43f28e6c32
Jonas Krogell <jonas.krogell@netrounds.com>
2018-12-28 17:19:19 +0100

    hack to solve 3b

--- 3.py
+++ 3.py
@@ -3,21 +3,37 @@ import sys
 
 fabric = [x[:] for x in [[0] * 1000] * 1000]
 
+claims = sys.stdin.read().strip().split('\n')
 
 # claim format: #1242 @ 792,499: 10x19
-for row in sys.stdin.read().strip().split('\n'):
-    parts = row.split(' ')
-    claim = parts[0]
+for claim in claims:
+    parts = claim.split(' ')
+    claim_id = parts[0]
     start_x = int(parts[2].split(',')[0])
     start_y = int(parts[2].split(',')[1].replace(':', ''))
     size_x = int(parts[3].split('x')[0])
     size_y = int(parts[3].split('x')[1])
 
-    print(row, claim, start_x, start_y, size_x, size_y)
-
     for x in range(start_x, start_x + size_x):
         for y in range(start_y, start_y + size_y):
             fabric[x][y] += 1
+
+# hack copy-paste checking
+for claim in claims:
+    parts = claim.split(' ')
+    claim_id = parts[0]
+    start_x = int(parts[2].split(',')[0])
+    start_y = int(parts[2].split(',')[1].replace(':', ''))
+    size_x = int(parts[3].split('x')[0])
+    size_y = int(parts[3].split('x')[1])
+
+    clean = True
+    for x in range(start_x, start_x + size_x):
+        for y in range(start_y, start_y + size_y):
+            if fabric[x][y] != 1:
+                clean = False
+    if clean:
+        print('Claim %s is a clean claim' % (claim_id))
 
 
 overlaps = 0
