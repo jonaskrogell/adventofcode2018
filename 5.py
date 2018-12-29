30087088b6fb1d5dff5fe1167274a4ac88c7c699
Jonas Krogell <jonas.krogell@netrounds.com>
2018-12-29 10:59:41 +0100

    solved 5b by bruteforce

--- 5.py
+++ 5.py
@@ -13,30 +13,42 @@ def test(a, b):
         return True
     return False
 
-new_polymers = ''
-loops = 0
-while len(new_polymers) != len(polymers):
-    loops += 1
-#    print(len(polymers))
-    if len(new_polymers) > 0:
-        polymers = new_polymers
+def collaps(polymers):
     new_polymers = ''
-    skip_next = False
-    for x in range(len(polymers) - 1):
-        if skip_next:
-            skip_next = False
-            continue
-        if test(polymers[x], polymers[x+1]):
-#            print('Removes %s and %s' % (polymers[x], polymers[x+1]))
-            skip_next = True
-            continue
-        else:
-            new_polymers += polymers[x]
-    # Add the last char
-    if not skip_next:
-        new_polymers += polymers[-1]
+    loops = 0
+    while len(new_polymers) != len(polymers):
+        loops += 1
+    #    print(len(polymers))
+        if len(new_polymers) > 0:
+            polymers = new_polymers
+        new_polymers = ''
+        skip_next = False
+        for x in range(len(polymers) - 1):
+            if skip_next:
+                skip_next = False
+                continue
+            if test(polymers[x], polymers[x+1]):
+    #            print('Removes %s and %s' % (polymers[x], polymers[x+1]))
+                skip_next = True
+                continue
+            else:
+                new_polymers += polymers[x]
+        # Add the last char
+        if not skip_next:
+            new_polymers += polymers[-1]
+    print('Loops: %i' % loops)
+    return polymers
 
-print('Loops: %i' % loops)
-print('Final:')
-print(polymers)
-print(len(polymers))
+
+collapsed = collaps(polymers)
+print('Native: %i' % len(collapsed))
+
+results = {}
+for x in 'abcdefgjhijklmnopqrstuvwxyz':
+    print('Removing %s' % x)
+    removed_polymer = polymers.replace(x, '').replace(x.upper(), '')
+    collapsed = collaps(removed_polymer)
+    print('%s: %i' % (x, len(collapsed)))
+    results[x] = len(collapsed)
+
+print('Length of %i found' % (min(results.values())))
