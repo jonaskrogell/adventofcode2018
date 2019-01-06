5c4c7179529cb33fb1f6534eacfb1b989ebefcf0
Jonas Krogell <jonas.krogell@netrounds.com>
2019-01-06 18:57:37 +0100

    solved 13b and fixed a strip-issue in reading the map

--- 13.py
+++ 13.py
@@ -11,11 +11,15 @@ def printMap(map, carts=[]):
         for x in range(len(map[y])):
             for cart in carts:
                 if cart['x'] == x and cart['y'] == y:
-                    print(cart['direction'], end='')
+                    if cart['collided']:
+                        print('X', end='')
+                    else:
+                        print(cart['direction'], end='')
                     break
             else:
                 print(map[y][x], end='')
         print()
+    return
 
 
 def moveCart(cart):
@@ -29,6 +33,7 @@ def moveCart(cart):
         cart['y'] += 1
     else:
         raise Exception('Unclear what happend')
+    return
 
 
 def turnCart(cart, move):
@@ -38,23 +43,36 @@ def turnCart(cart, move):
         cart['direction'] = directions[current - 1]
     if move == 'right':
         cart['direction'] = directions[(current + 1) % 4]
+    return
 
 
 def checkCollections(carts):
     cords = set()
+    collision_cords = []
     for cart in carts:
+        if cart['collided'] == True:
+            continue
         if (cart['x'], cart['y']) in cords:
-            raise Exception('Collision at %ix%i!' % (cart['x'], cart['y']))
+            collision_cords.append((cart['x'], cart['y']))
+            print('Collision at %ix%i!' % (cart['x'], cart['y']))
         cords.add((cart['x'], cart['y']))
+    if len(collision_cords) == 0:
+        return
+    for cart in carts:
+        if (cart['x'], cart['y']) in collision_cords:
+            cart['collided'] = True
+    return
 
 map = []
 carts = []
 y = 0
-for row in sys.stdin.read().strip().split('\n'):
+for row in sys.stdin.read().split('\n'):
+    if len(row.strip()) == 0:
+        continue
     new_row = []
     for x in range(len(row)):
         if row[x] in ['v', '^', '<', '>']:
-            carts.append({'direction': row[x], 'x': x, 'y': y, 'states': ['left', 'straight', 'right']})
+            carts.append({'direction': row[x], 'x': x, 'y': y, 'states': ['left', 'straight', 'right'], 'collided': False})
             if row[x] in ['v', '^']: new_row.append('|')
             if row[x] in ['<', '>']: new_row.append('-')
         else:
@@ -71,8 +89,9 @@ printMap(map, carts)
 
 tick = 0
 while True:
-#    print('Tick: %i' % tick)
     for cart in sorted(carts, key=lambda k: (k['y'], k['x'])):
+        if cart['collided']:
+            continue
         if map[cart['y']][cart['x']] == '|':
             pass
         elif map[cart['y']][cart['x']] == '-':
@@ -92,11 +111,19 @@ while True:
             cart['states'].append(move)
             turnCart(cart, move)
         else:
+            printMap(map)
             print(cart)
-            print(x, y, map[cart['y']][cart['x']])
+            print(tick, cart['x'], cart['y'], map[cart['y']][cart['x']])
             raise Exception('What happend here?')
         moveCart(cart)
         checkCollections(carts)
-        continue
+
+
+#    print('Tick: %i' % tick)
 #    printMap(map, carts)
+    if len(list(filter(lambda x: x['collided'] == False, carts))) <= 1:
+        print('At tick %i we have one or no cart left.' % tick)
+        print('Remaining cart:')
+        print(list(filter(lambda x: x['collided'] == False, carts)))
+        break
     tick += 1
