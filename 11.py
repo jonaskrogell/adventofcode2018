fdae6bfe3e918350de6031f0b5a29d4dace7cfbb
Jonas Krogell <jonas.krogell@netrounds.com>
2019-01-06 09:59:40 +0100

    speed up by caching grid

--- 11.py
+++ 11.py
@@ -1,3 +1,4 @@
+
 def getPower(x, y, serial, correct_power=None):
     #Find the fuel cell's rack ID, which is its X coordinate plus 10.
     power = x + 10
@@ -8,11 +9,7 @@ def getPower(x, y, serial, correct_power=None):
     #Set the power level to itself multiplied by the rack ID.
     power = power * (x + 10)
     #Keep only the hundreds digit of the power level (so 12345 becomes 3; numbers with no hundreds digit become 0).
-    if abs(power) >= 100:
-#        power = int(str(power)[-3])
-        power = int(power/100 % 10)
-    else:
-        power = 0
+    power = int(power/100 % 10)
     #Subtract 5 from the power level.
     power -= 5
     if correct_power is not None:
@@ -21,10 +18,21 @@ def getPower(x, y, serial, correct_power=None):
     return power
 
 
-def getMaxSquare(size, serial, correct_sum=None, correct_x=None, correct_y=None):
+def getGrid(serial):
+    grid = []
+    for y in range(300 + 1):
+        row = []
+        for x in range(300 + 1):
+            row.append(getPower(x, y, serial))
+        grid.append(row)
+    return grid
+
+
+def getMaxSquare(grid, size, correct_sum=None, correct_x=None, correct_y=None):
     max_sum = None
     max_x = None
     max_y = None
+
     for y in range(1, 300 - size + 2):
         prev_s = None
         for x in range(1, 300 - size + 2):
@@ -33,12 +41,12 @@ def getMaxSquare(size, serial, correct_sum=None, correct_x=None, correct_y=None)
                 s = 0
                 for s_y in range(size):
                     for s_x in range(size):
-                        s += getPower(x + s_x, y + s_y, serial)
+                        s += grid[y + s_y][x + s_x]
             else:
                 s = prev_s
                 for s_y in range(size):
-                    s -= getPower(x - 1 , y + s_y, serial)
-                    s += getPower(x + size - 1, y + s_y, serial)
+                    s -= grid[y + s_y][x - 1]
+                    s += grid[y + s_y][x + size - 1]
             prev_s = s
             if max_sum is None or s > max_sum:
                 max_sum = s
@@ -46,28 +54,28 @@ def getMaxSquare(size, serial, correct_sum=None, correct_x=None, correct_y=None)
                 max_y = y
     if correct_sum is not None:
         if max_sum != correct_sum or max_x != correct_x or max_y != correct_y:
-            raise Exception('Testing failed for size %i serial %i. Sum: %i %i X: %i %i Y: %i %i' % (size, serial, correct_sum, max_sum, correct_x, max_x, correct_y, max_y))
+            raise Exception('Testing failed for size %i. Sum: %i %i X: %i %i Y: %i %i' % (size, correct_sum, max_sum, correct_x, max_x, correct_y, max_y))
     return max_sum, max_x, max_y, size
 
 
 # tests
-getPower(3,5, 8, 4)
-getPower(122,79, 57, -5)
-getPower(217,196, 39, 0)
-getPower(101,153, 71, 4)
+getPower(3, 5, 8, 4)
+getPower(122, 79, 57, -5)
+getPower(217, 196, 39, 0)
+getPower(101, 153, 71, 4)
 
-getMaxSquare(3, 18, 29, 33, 45)
-getMaxSquare(3, 42, 30, 21, 61)
-getMaxSquare(16, 18, 113, 90,269)
-getMaxSquare(12, 42, 119, 232,251)
+getMaxSquare(getGrid(18), 3, 29, 33, 45)
+getMaxSquare(getGrid(42), 3, 30, 21, 61)
+getMaxSquare(getGrid(18), 16, 113, 90,269)
+getMaxSquare(getGrid(42), 12, 119, 232,251)
 print('Tests passed.')
 
 # Jonas serial
-serial = 7803
-max_sum, max_x, max_y, size = getMaxSquare(3, serial)
+grid = getGrid(7803)
+max_sum, max_x, max_y, size = getMaxSquare(grid, 3)
 print('Max sum of %i found at %i,%i of square size %i' % (max_sum, max_x, max_y, size))
 
 print('Brute force the size:')
 for size in range(300):
-    max_sum, max_x, max_y, size = getMaxSquare(size, serial)
+    max_sum, max_x, max_y, size = getMaxSquare(grid, size)
     print('Max sum of %i found at %i,%i of square size %i' % (max_sum, max_x, max_y, size))
