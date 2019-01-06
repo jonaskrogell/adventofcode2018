989266699385f3f0f4f450eb73fa192820789253
Jonas Krogell <jonas.krogell@netrounds.com>
2019-01-06 16:10:02 +0100

    12b solved

--- 12.py
+++ 12.py
@@ -20,14 +20,19 @@ def getSum(pots, offset):
 
 
 offset = 0
+total_gens = 50000000000
 print(0, pots)
-for gen in range(1, 20 + 1):
+for gen in range(1, total_gens + 1):
     # pad on empty pots
-    while pots[:5] != '.....':
+#    print(pots)
+    while pots[:3] != '...':
         pots = '.' + pots
+#        print('left padding')
         offset -= 1
-    while pots[-5:] != '.....':
+    while pots[-3:] != '...':
         pots = pots + '.'
+#        print('right padding')
+#    print(pots)
     ng_pots = '..'
     for nr in range(2, len(pots) - 2):
         pattern = pots[nr - 2:nr + 3]
@@ -38,12 +43,15 @@ for gen in range(1, 20 + 1):
 
     if pots.strip('.') == ng_pots.strip('.'):
         print('After gen %i they are identical' % gen)
+        p = pots.strip('.')
+        print('Shift: %i' % (ng_pots.index(p) - pots.index(p) ))
+        offset += (ng_pots.index(p) - pots.index(p) ) * ( total_gens - gen + 1)
         break
     pots = ng_pots
-    print(gen, pots)
-    if gen % 1000 == 0:
-#        print(gen, pots)
-        print('Sum: %i' % (getSum(pots, offset)))
+    print(gen, offset, pots)
+    if gen == 20:
+        print(gen, pots)
+        print('Round 20 Sum: %i' % (getSum(pots, offset)))
 
 
-print('Sum: %i' % (getSum(pots, offset)))
+print('Sum after %i rounds: %i' % (total_gens, getSum(pots, offset)))
