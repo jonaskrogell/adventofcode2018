ba76703d68b3bd7efad39bed55588cd6810051f9
Jonas Krogell <jonas.krogell@netrounds.com>
2019-01-12 14:50:27 +0100

    minor fixes

--- 14.py
+++ 14.py
@@ -1,33 +1,40 @@
 def printScoreboard(scoreboard, elves):
     for pos in range(len(scoreboard)):
         for elve in elves:
-            if elves[elve] == pos:
+            if elve == pos:
                 print('<%i>  ' % scoreboard[pos], end='')
                 break
         else:
             print('%i  ' % scoreboard[pos], end='')
     print('')
 
-def doRecepies(nr):
-    print('Doing %i recepies' % nr)
-    elves = {}
+def doRecepies(recepies):
+    print('Doing %i recepies' % recepies)
+    elves = []
     for elve in range(2):
-        elves[elve] = elve
+        elves.append(elve)
     scoreboard = [3, 7]
     while True:
 #        printScoreboard(scoreboard, elves)
         sum = 0
         for elve in elves:
-            sum += scoreboard[elves[elve]]
+            sum += scoreboard[elve]
+        # nrs = []
+        # while sum > 0:
+        #     nrs.append(sum % 10)
+        #     sum = int((sum - sum % 10)/10)
+        # nrs.reverse()
+        # for nr in nrs:
+        #     scoreboard.append(nr)
         for digit in str(sum):
             scoreboard.append(int(digit))
-        for elve in elves:
+        for elve in range(len(elves)):
             elves[elve] = (elves[elve] + scoreboard[elves[elve]] + 1) % len(scoreboard)
 
-        if len(scoreboard) >= nr + 10:
+        if len(scoreboard) >= recepies + 10:
             break
 #    printScoreboard(scoreboard, elves)
-    return ''.join(map(str, scoreboard[nr:nr+10]))
+    return ''.join(map(str, scoreboard[recepies:recepies+10]))
 
 print(doRecepies(9) == '5158916779')
 print(doRecepies(5) == '0124515891')
